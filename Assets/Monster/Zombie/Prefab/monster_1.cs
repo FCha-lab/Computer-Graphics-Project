ea380c9acf6a2e4fafd3765fecf741f142ad55e0
YCB205 <ckd5310@naver.com>
2023-06-15 02:47:37 +0900

    양창배
    몬스터 화면전환 구현
    유저 몬스터 사망액션 구현
    (사운드 X)

--- Assets/Monster/Zombie/Prefab/monster_1.cs
+++ Assets/Monster/Zombie/Prefab/monster_1.cs
@@ -8,7 +8,21 @@ public class monster_1 : MonoBehaviour
     NavMeshAgent nav;
     GameObject target;
     Animator animator;
-    public float stoppingDistance = 10f; // 멈추는 거리
+    public float stoppingDistance = 20f; // 멈추는 거리
+    private bool isNavFrozen = false; // NavMeshAgent를 움직이지 않게 하는 플래그
+    private float navFreezeDuration = 4f; // NavMeshAgent를 얼린 시간
+    private float getPlayerDistance = 3.5f; // 플레이어를 잡는 거리
+
+    public AudioClip getPlayerClip; // getPlayer 오디오 클립
+    public AudioClip playerScreamingClip; // playerScreaming 오디오 클립
+    public AudioClip surprisedSoundClip; // surprisedSound 오디오 클립
+    public AudioClip badEndingsongClip; // badEndingsong 오디오 클립
+    private AudioSource audioSource; // 음악 재생용 AudioSource
+    private AudioSource getPlayerAudioSource; // getPlayer 오디오 클립을 재생하는 AudioSource
+    private AudioSource playerScreamingAudioSource; // playerScreaming 오디오 클립을 재생하는 AudioSource
+    private AudioSource surprisedSoundAudioSource; // surprisedSound 오디오 클립을 재생하는 AudioSource
+    private AudioSource badEndingsongAudioSource; // badEndingsong 오디오 클립을 재생하는 AudioSource
+
     // Use this for initialization
     void Start()
     {
@@ -16,25 +30,59 @@ public class monster_1 : MonoBehaviour
         target = GameObject.Find("player");
         animator = GetComponent<Animator>();
 
+        audioSource = GetComponent<AudioSource>(); // AudioSource 컴포넌트 가져오기
+
+        getPlayerAudioSource = gameObject.AddComponent<AudioSource>();
+        getPlayerAudioSource.clip = getPlayerClip;
+
+        playerScreamingAudioSource = gameObject.AddComponent<AudioSource>();
+        playerScreamingAudioSource.clip = playerScreamingClip;
+
+        surprisedSoundAudioSource = gameObject.AddComponent<AudioSource>();
+        surprisedSoundAudioSource.clip = surprisedSoundClip;
+
+        badEndingsongAudioSource = gameObject.AddComponent<AudioSource>();
+        badEndingsongAudioSource.clip = badEndingsongClip;
+
+        StartCoroutine(FreezeNavForDuration(navFreezeDuration));
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (nav.destination != target.transform.position)
+        if (!isNavFrozen)
         {
-            nav.SetDestination(target.transform.position);
+            if (nav.destination != target.transform.position)
+            {
+                nav.SetDestination(target.transform.position);
+            }
+            else
+            {
+                nav.SetDestination(transform.position);
+            }
         }
-        else
-        {
-            nav.SetDestination(transform.position);
-        }
+
         float distanceToTarget = Vector3.Distance(transform.position, target.transform.position);
         if (distanceToTarget < stoppingDistance)
         {
             // 플레이어를 추격 중인 경우 애니메이션을 재생
             animator.SetBool("run", true);
             nav.isStopped = false;
+
+            if (distanceToTarget < getPlayerDistance)
+            {
+                animator.SetBool("atack", true);
+
+                // 플레이어의 카메라를 바라보도록 회전
+                Vector3 targetPosition = new Vector3(nav.transform.position.x, nav.transform.position.y + 2, nav.transform.position.z);
+                target.GetComponentInChildren<Camera>().transform.LookAt(targetPosition);
+                audioSource.Stop();
+                // 오디오 클립들을 동시에 재생
+                getPlayerAudioSource.PlayOneShot(getPlayerClip);
+             //   playerScreamingAudioSource.PlayOneShot(playerScreamingClip);
+               // surprisedSoundAudioSource.PlayOneShot(surprisedSoundClip);
+               // badEndingsongAudioSource.PlayOneShot(badEndingsongClip);
+            }
         }
         else
         {
@@ -42,6 +90,14 @@ public class monster_1 : MonoBehaviour
             animator.SetBool("run", false);
             nav.isStopped = true;
 
+            animator.SetBool("atack", false);
         }
+    }
+
+    IEnumerator FreezeNavForDuration(float duration)
+    {
+        isNavFrozen = true;
+        yield return new WaitForSeconds(duration);
+        isNavFrozen = false;
     }
 }
--- Assets/camera/MouseMove.cs
+++ Assets/camera/MouseMove.cs
@@ -4,26 +4,135 @@ using UnityEngine;
 
 public class MouseMove : MonoBehaviour
 {
-    public float sensitivity = 250f; // 마우스 민감도
+    public float sensitivity = 250f;
     public float rotationX;
     public float rotationY;
 
+    public GameObject targetObject;
+    public float targetDistance = 20f;
+    public float returnSpeed = 0.2f;
+    public float moveSpeed = 0.1f;
+
+    public AudioClip lookAMonsterClip;
+    public AudioClip lookEnterMonster1Clip;
+    public AudioClip chaseAfterPlayerClip;
+    public AudioClip reliefPlayerSound; // 쫒음 해제를 나타내는 오디오 클립
+
+    private bool isLookingAtObject = false;
+    private bool canRotate = true;
+    private bool hasLookedAtObject = false;
+    private Quaternion originalRotation;
+    private Vector3 originalPosition;
+
+    private AudioSource audioSource;
+    private bool isPlayingChaseMusic = false;
+    private float startVolume;
+
+    void Start()
+    {
+        originalRotation = transform.rotation;
+        originalPosition = transform.position;
+        targetObject = GameObject.Find("Zombie");
+
+        audioSource = GetComponent<AudioSource>();
+    }
+
     void Update()
     {
-        float mouseMoveX = Input.GetAxis("Mouse X"); // 마우스 X축 움직임값을 받아서 mouseMoveX 변수에 저장
-        float mouseMoveY = Input.GetAxis("Mouse Y"); // 마우스 Y축 움직임값을 받아서 mouseMoveY 변수에 저장
-        rotationY += mouseMoveX * sensitivity * Time.deltaTime; // rotationY 변수의 값은 rotationY + (mouseMoveX * 마우스 민감도 * Time.deltaTime)
-        rotationX += mouseMoveY * sensitivity * Time.deltaTime; // rotationX 변수의 값은 rotationX + (mouseMoveY * 마우스 민감도 * Time.deltaTime)
-
-        if (rotationX < -30f)
+        if (canRotate)
         {
-            rotationX = -30f;
-        }
-        if (rotationX > 35f)
-        {
-            rotationX = 35f;
+            float mouseMoveX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+            rotationY += mouseMoveX;
+
+            float mouseMoveY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+            rotationX -= mouseMoveY;
+
+            rotationX = Mathf.Clamp(rotationX, -90f, 90f);
+
+            transform.rotation = Quaternion.Euler(rotationX, rotationY, 0f);
         }
 
-        transform.eulerAngles = new Vector3(-rotationX, rotationY, 0); // rotationX값, rotationY값, Z는 0값을 Vector3 형식으로 변환하고, transform 오일러각에 저장함.
+        if (!isLookingAtObject && !hasLookedAtObject && Vector3.Distance(transform.position, targetObject.transform.position) <= targetDistance)
+        {
+            isLookingAtObject = true;
+            StartCoroutine(LookAtObject());
+        }
+    }
+
+    IEnumerator LookAtObject()
+    {
+        Quaternion targetRotation = Quaternion.LookRotation(targetObject.transform.position - transform.position);
+        float t = 0f;
+
+        canRotate = false;
+
+        audioSource.clip = lookAMonsterClip;
+        audioSource.Play();
+
+        AudioSource.PlayClipAtPoint(lookEnterMonster1Clip, transform.position);
+
+        while (t < 1f)
+        {
+            t += Time.deltaTime * returnSpeed;
+            Quaternion newRotation = Quaternion.Slerp(transform.rotation, targetRotation, t);
+            transform.rotation = Quaternion.Euler(newRotation.eulerAngles.x, newRotation.eulerAngles.y, 0f);
+
+            transform.position = Vector3.Lerp(originalPosition, targetObject.transform.position, t * moveSpeed);
+
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(3f);
+
+        canRotate = true;
+        isLookingAtObject = false;
+        hasLookedAtObject = true;
+
+        audioSource.Stop();
+
+        transform.position = originalPosition;
+
+        StartCoroutine(PlayChaseAfterPlayerClip());
+    }
+
+    IEnumerator PlayChaseAfterPlayerClip()
+    {
+        yield return new WaitForSeconds(0f);
+
+        if (Vector3.Distance(transform.position, targetObject.transform.position) <= targetDistance)
+        {
+            isPlayingChaseMusic = true;
+            audioSource.clip = chaseAfterPlayerClip;
+            audioSource.Play();
+        }
+    }
+
+    void LateUpdate()
+    {
+        if (isPlayingChaseMusic && Vector3.Distance(transform.position, targetObject.transform.position) > targetDistance)
+        {
+            StartCoroutine(FadeOutMusic(4f));
+        }
+    }
+
+    IEnumerator FadeOutMusic(float fadeTime)
+    {
+        startVolume = audioSource.volume;
+        float t = 0f;
+
+        while (t < fadeTime)
+        {
+            t += Time.deltaTime;
+            audioSource.volume = Mathf.Lerp(startVolume, 0f, t / fadeTime);
+            yield return null;
+        }
+
+        audioSource.Stop();
+        isPlayingChaseMusic = false;
+
+        audioSource.volume = startVolume; // 볼륨을 원래대로 되돌림
+
+        audioSource.clip = reliefPlayerSound;
+        audioSource.Play();
     }
 }
